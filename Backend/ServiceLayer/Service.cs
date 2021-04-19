9cf52cd941ca53499a5e88d8bfb3525ddd1b968f
Guy Shtar <goolig@gmail.com>
2021-04-19 11:22:29 +0300

    Adding service.cs for MS2

--- Backend/ServiceLayer/Service.cs
+++ Backend/ServiceLayer/Service.cs
@@ -22,46 +22,36 @@ namespace IntroSE.Kanban.Backend.ServiceLayer
         {
             throw new NotImplementedException();
         }
-        ///<summary>This method registers a new user to the system.</summary>
-        ///<param name="email">the user e-mail address, used as the username for logging the system.</param>
-        ///<param name="password">the user password.</param>
-        ///<returns cref="Response">The response of the action</returns>
-        public Response Register(string email, string password)
-        {
-            throw new NotImplementedException();
-        }
+
         /// <summary>
         /// Log in an existing user
         /// </summary>
-        /// <param name="email">The email address of the user to login</param>
+        /// <param name="userEmail">The email address of the user to login</param>
         /// <param name="password">The password of the user to login</param>
         /// <returns>A response object with a value set to the user, instead the response should contain a error message in case of an error</returns>
-        public Response<User> Login(string email, string password)
+        public Response<User> Login(string userEmail, string password)
         {
             throw new NotImplementedException();
         }
         /// <summary>        
-        /// Log out an logged in user. 
+        /// Log out an logged-in user. 
         /// </summary>
-        /// <param name="email">The email of the user to log out</param>
+        /// <param name="userEmail">The email of the user to log out</param>
         /// <returns>A response object. The response should contain a error message in case of an error</returns>
-        public Response Logout(string email)
-        {
-            throw new NotImplementedException();
-        }
-        private void ValidateUserLoggin(string email)
+        public Response Logout(string userEmail)
         {
             throw new NotImplementedException();
         }
         /// <summary>
         /// Limit the number of tasks in a specific column
         /// </summary>
-        /// <param name="email">The email address of the user, must be logged in</param>
+        /// <param name="userEmail">Email of the current user. Must be logged in</param>
+        /// <param name="creatorEmail">Email of the board creator</param>
         /// <param name="boardName">The name of the board</param>
         /// <param name="columnOrdinal">The column ID. The first column is identified by 0, the ID increases by 1 for each column</param>
         /// <param name="limit">The new limit value. A value of -1 indicates no limit.</param>
         /// <returns>A response object. The response should contain a error message in case of an error</returns>
-        public Response LimitColumn(string email, string boardName, int columnOrdinal, int limit)
+        public Response LimitColumn(string userEmail, string creatorEmail, string boardName, int columnOrdinal, int limit)
         {
             throw new NotImplementedException();
         }
@@ -69,11 +59,12 @@ namespace IntroSE.Kanban.Backend.ServiceLayer
         /// <summary>
         /// Get the limit of a specific column
         /// </summary>
-        /// <param name="email">The email address of the user, must be logged in</param>
+        /// <param name="userEmail">Email of the current user. Must be logged in</param>
+        /// <param name="creatorEmail">Email of the board creator</param>
         /// <param name="boardName">The name of the board</param>
         /// <param name="columnOrdinal">The column ID. The first column is identified by 0, the ID increases by 1 for each column</param>
         /// <returns>The limit of the column.</returns>
-        public Response<int> GetColumnLimit(string email, string boardName, int columnOrdinal)
+        public Response<int> GetColumnLimit(string userEmail, string creatorEmail, string boardName, int columnOrdinal)
         {
             throw new NotImplementedException();
         }
@@ -81,11 +72,12 @@ namespace IntroSE.Kanban.Backend.ServiceLayer
         /// <summary>
         /// Get the name of a specific column
         /// </summary>
-        /// <param name="email">The email address of the user, must be logged in</param>
+        /// <param name="userEmail">Email of the current user. Must be logged in</param>
+        /// <param name="creatorEmail">Email of the board creator</param>
         /// <param name="boardName">The name of the board</param>
         /// <param name="columnOrdinal">The column ID. The first column is identified by 0, the ID increases by 1 for each column</param>
         /// <returns>The name of the column.</returns>
-        public Response<string> GetColumnName(string email, string boardName, int columnOrdinal)
+        public Response<string> GetColumnName(string userEmail, string creatorEmail, string boardName, int columnOrdinal)
         {
             throw new NotImplementedException();
         }
@@ -93,106 +85,167 @@ namespace IntroSE.Kanban.Backend.ServiceLayer
         /// <summary>
         /// Add a new task.
         /// </summary>
-        /// <param name="email">Email of the user. The user must be logged in.</param>
+		/// <param name="userEmail">Email of the current user. Must be logged in</param>
+        /// <param name="creatorEmail">Email of the board creator</param>
         /// <param name="boardName">The name of the board</param>
         /// <param name="title">Title of the new task</param>
         /// <param name="description">Description of the new task</param>
         /// <param name="dueDate">The due date if the new task</param>
         /// <returns>A response object with a value set to the Task, instead the response should contain a error message in case of an error</returns>
-        public Response<Task> AddTask(string email, string boardName, string title, string description, DateTime dueDate)
+        public Response<Task> AddTask(string userEmail,string creatorEmail, string boardName, string title, string description, DateTime dueDate)
         {
             throw new NotImplementedException();
         }
         /// <summary>
         /// Update the due date of a task
         /// </summary>
-        /// <param name="email">Email of the user. Must be logged in</param>
+        /// <param name="userEmail">Email of the current user. Must be logged in</param>
+        /// <param name="creatorEmail">Email of the board creator</param>
         /// <param name="boardName">The name of the board</param>
         /// <param name="columnOrdinal">The column ID. The first column is identified by 0, the ID increases by 1 for each column</param>
         /// <param name="taskId">The task to be updated identified task ID</param>
         /// <param name="dueDate">The new due date of the column</param>
         /// <returns>A response object. The response should contain a error message in case of an error</returns>
-        public Response UpdateTaskDueDate(string email, string boardName, int columnOrdinal, int taskId, DateTime dueDate)
+        public Response UpdateTaskDueDate(string userEmail, string creatorEmail, string boardName, int columnOrdinal, int taskId, DateTime dueDate)
         {
             throw new NotImplementedException();
         }
         /// <summary>
         /// Update task title
         /// </summary>
-        /// <param name="email">Email of user. Must be logged in</param>
+        /// <param name="userEmail">Email of the current user. Must be logged in</param>
+        /// <param name="creatorEmail">Email of the board creator</param>
         /// <param name="boardName">The name of the board</param>
         /// <param name="columnOrdinal">The column ID. The first column is identified by 0, the ID increases by 1 for each column</param>
         /// <param name="taskId">The task to be updated identified task ID</param>
         /// <param name="title">New title for the task</param>
         /// <returns>A response object. The response should contain a error message in case of an error</returns>
-        public Response UpdateTaskTitle(string email, string boardName, int columnOrdinal, int taskId, string title)
+        public Response UpdateTaskTitle(string userEmail, string creatorEmail, string boardName, int columnOrdinal, int taskId, string title)
         {
             throw new NotImplementedException();
         }
         /// <summary>
         /// Update the description of a task
         /// </summary>
-        /// <param name="email">Email of user. Must be logged in</param>
+        /// <param name="userEmail">Email of the current user. Must be logged in</param>
+        /// <param name="creatorEmail">Email of the board creator</param>
         /// <param name="boardName">The name of the board</param>
         /// <param name="columnOrdinal">The column ID. The first column is identified by 0, the ID increases by 1 for each column</param>
         /// <param name="taskId">The task to be updated identified task ID</param>
         /// <param name="description">New description for the task</param>
         /// <returns>A response object. The response should contain a error message in case of an error</returns>
-        public Response UpdateTaskDescription(string email, string boardName, int columnOrdinal, int taskId, string description)
+        public Response UpdateTaskDescription(string userEmail, string creatorEmail, string boardName, int columnOrdinal, int taskId, string description)
         {
             throw new NotImplementedException();
         }
         /// <summary>
         /// Advance a task to the next column
         /// </summary>
-        /// <param name="email">Email of user. Must be logged in</param>
+        /// <param name="userEmail">Email of the current user. Must be logged in</param>
+        /// <param name="creatorEmail">Email of the board creator</param>
         /// <param name="boardName">The name of the board</param>
         /// <param name="columnOrdinal">The column ID. The first column is identified by 0, the ID increases by 1 for each column</param>
         /// <param name="taskId">The task to be updated identified task ID</param>
         /// <returns>A response object. The response should contain a error message in case of an error</returns>
-        public Response AdvanceTask(string email, string boardName, int columnOrdinal, int taskId)
+        public Response AdvanceTask(string userEmail, string creatorEmail, string boardName, int columnOrdinal, int taskId)
         {
             throw new NotImplementedException();
         }
         /// <summary>
         /// Returns a column given it's name
         /// </summary>
-        /// <param name="email">Email of the user. Must be logged in</param>
+        /// <param name="userEmail">Email of the current user. Must be logged in</param>
+        /// <param name="creatorEmail">Email of the board creator</param>
         /// <param name="boardName">The name of the board</param>
         /// <param name="columnOrdinal">The column ID. The first column is identified by 0, the ID increases by 1 for each column</param>
         /// <returns>A response object with a value set to the Column, The response should contain a error message in case of an error</returns>
-        public Response<IList<Task>> GetColumn(string email, string boardName, int columnOrdinal)
+        public Response<IList<Task>> GetColumn(string userEmail, string creatorEmail, string boardName, int columnOrdinal)
         {
             throw new NotImplementedException();
         }
+
+
         /// <summary>
-        /// Adds a board to the specific user.
+        /// Creates a new board for the logged-in user.
         /// </summary>
-        /// <param name="email">Email of the user. Must be logged in</param>
-        /// <param name="name">The name of the new board</param>
+        /// <param name="userEmail">Email of the current user. Must be logged in</param>
+        /// <param name="boardName">The name of the new board</param>
         /// <returns>A response object. The response should contain a error message in case of an error</returns>
-        public Response AddBoard(string email, string name)
+        public Response AddBoard(string userEmail, string boardName)
         {
             throw new NotImplementedException();
         }
+
         /// <summary>
-        /// Removes a board to the specific user.
+        /// Adds a board created by another user to the logged-in user. 
         /// </summary>
-        /// <param name="email">Email of the user. Must be logged in</param>
-        /// <param name="name">The name of the board</param>
+        /// <param name="userEmail">Email of the current user. Must be logged in</param>
+        /// <param name="creatorEmail">Email of the board creator</param>
+        /// <param name="boardName">The name of the new board</param>
         /// <returns>A response object. The response should contain a error message in case of an error</returns>
-        public Response RemoveBoard(string email, string name)
+        public Response JoinBoard(string userEmail, string creatorEmail, string boardName)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Removes a board.
+        /// </summary>
+        /// <param name="userEmail">Email of the current user. Must be logged in</param>
+        /// <param name="creatorEmail">Email of the board creator. Must be logged in</param>
+        /// <param name="boardName">The name of the board</param>
+        /// <returns>A response object. The response should contain a error message in case of an error</returns>
+        public Response RemoveBoard(string userEmail, string creatorEmail, string boardName)
         {
             throw new NotImplementedException();
         }
         /// <summary>
-        /// Returns all the In progress tasks of the user.
+        /// Returns all the in-progress tasks of the logged-in user is assigned to.
         /// </summary>
-        /// <param name="email">Email of the user. Must be logged in</param>
+        /// <param name="userEmail">Email of the logged in user</param>
         /// <returns>A response object with a value set to the list of tasks, The response should contain a error message in case of an error</returns>
-        public Response<IList<Task>> InProgressTasks(string email)
+        public Response<IList<Task>> InProgressTasks(string userEmail)
         {
             throw new NotImplementedException();
         }
+
+
+        /// <summary>
+        /// Registers a new user.
+        /// </summary>
+        /// <param name="userEmail">The email address of the user to register</param>
+        /// <param name="password">The password of the user to register</param>
+        /// <returns>A response object. The response should contain a error message in case of an error<returns>
+        public Response Register(string userEmail, string password)
+        {
+            throw new NotImplementedException();
+        }
+
+
+        /// <summary>
+        /// Assigns a task to a user
+        /// </summary>
+        /// <param name="userEmail">Email of the current user. Must be logged in</param>
+        /// <param name="creatorEmail">Email of the board creator</param>
+        /// <param name="boardName">The name of the board</param>
+        /// <param name="columnOrdinal">The column ID. The first column is identified by 0, the ID increases by 1 for each column</param>
+        /// <param name="taskId">The task to be updated identified task ID</param>        
+        /// <param name="emailAssignee">Email of the user to assign to task to</param>
+        /// <returns>A response object. The response should contain a error message in case of an error</returns>
+        public Response AssignTask(string userEmail, string creatorEmail, string boardName, int columnOrdinal, int taskId, string emailAssignee)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Returns the list of board of a user. The user must be logged-in. The function returns all the board names the user created or joined.
+        /// </summary>
+        /// <param name="userEmail">The email of the user. Must be logged-in.</param>
+        /// <returns>A response object with a value set to the board, instead the response should contain a error message in case of an error</returns>
+        public Response<IList<String>> GetBoardNames(string userEmail)
+        {
+            throw new NotImplementedException();
+        }
+
     }
 }
